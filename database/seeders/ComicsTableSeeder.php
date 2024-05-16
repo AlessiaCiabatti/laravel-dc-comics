96d88675b46f7147a8b34659d66471ad8ffb34a2
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-05-16 16:58:19 +0200

    [add] Seeder and [add] helper function generateSlug

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Comic;
+use App\Function\Helper;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class ComicsTableSeeder extends Seeder
     public function run(): void
     {
         $comics = config('comics');
-        dump($comics);
+        // dump($comics);
+
+        foreach($comics as $comic){
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            // con la funzione implode() convertiamo gli array di artisti e scrittori in stringhe separate da virgole
+            $new_comic->artists = implode(', ', $comic['artists']);
+            $new_comic->writers = implode(', ', $comic['writers']);
+
+            dump($new_comic);
+        }
     }
 }
